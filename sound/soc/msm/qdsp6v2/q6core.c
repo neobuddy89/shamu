f32318888990aa65699e4db3800ff51c2bcd43f7
Kenneth Westfield <kwestfie@codeaurora.org>
2013-12-09 13:39:21 -0800

    ASoC: msm: qdsp6v2: Fixup log message
    Fixed up the log message in aprv2_core_fn_q()
    and grouped the header files under the linux
    directory together.
    
    Signed-off-by: Kenneth Westfield <kwestfie@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sound/soc/msm/qdsp6v2/q6core.c
+++ sound/soc/msm/qdsp6v2/q6core.c
@@ -18,10 +18,10 @@
 #include <linux/mutex.h>
 #include <linux/sched.h>
 #include <linux/slab.h>
-#include <soc/qcom/smd.h>
 #include <linux/qdsp6v2/apr.h>
 #include <sound/q6core.h>
 #include <soc/qcom/ocmem.h>
+#include <soc/qcom/smd.h>
 
 #define TIMEOUT_MS 1000
 
@@ -42,7 +42,7 @@ static int32_t aprv2_core_fn_q(struct apr_client_data *data, void *priv)
 	int i, j;
 
 	if (data == NULL) {
-		pr_err("%s: data argument is null", __func__);
+		pr_err("%s: data argument is null\n", __func__);
 		return -EINVAL;
 	}
 
