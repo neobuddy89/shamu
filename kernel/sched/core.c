e935487ff81fc17ad0b15faefa5569e2db3b2989
Peter Zijlstra <peterz@infradead.org>
2014-01-14 11:23:34 +0800

    sched/deadline: Remove unused variables
    fix these new sparse warnings:
    
      >> kernel/sched/core.c:305:14: sparse: symbol 'sysctl_sched_dl_period' was not declared. Should it be static?
      >> kernel/sched/core.c:306:5: sparse: symbol 'sysctl_sched_dl_runtime' was not declared. Should it be static?
    
    Better still, they're completely unused so remove them.
    
    Reported-by: Fengguang Wu <fengguang.wu@intel.com>
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Cc: Juri Lelli <juri.lelli@gmail.com>
    Link: http://lkml.kernel.org/n/tip-ke0shkG7vMnzmcdqhhiymyem@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    [rameezmustafa@codeaurora.org: Port to msm-3.10]
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -348,15 +348,6 @@ __read_mostly int scheduler_running;
 int sysctl_sched_rt_runtime = 950000;
 
 /*
- * Maximum bandwidth available for all -deadline tasks and groups
- * (if group scheduling is configured) on each CPU.
- *
- * default: 5%
- */
-unsigned int sysctl_sched_dl_period = 1000000;
-int sysctl_sched_dl_runtime = 50000;
-
-/*
  * Number of sched_yield calls that result in a thread yielding
  * to itself before a sleep is injected in its next sched_yield call
  * Setting this to -1 will disable adding sleep in sched_yield
