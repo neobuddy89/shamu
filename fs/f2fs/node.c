c4b5ade0600094770cd8c8695bb7ef435becccff
Jaegeuk Kim <jaegeuk.kim@samsung.com>
2014-02-04 14:20:16 +0900

    f2fs: support 3.10
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -1056,6 +1056,7 @@ repeat:
 		f2fs_put_page(page, 1);
 		goto repeat;
 	}
+	mark_page_accessed(page);
 	return page;
 }
 
@@ -1110,6 +1111,7 @@ page_hit:
 		f2fs_put_page(page, 1);
 		return ERR_PTR(-EIO);
 	}
+	mark_page_accessed(page);
 	return page;
 }
 
