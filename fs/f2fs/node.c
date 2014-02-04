402fc2f20d8505611ac0f172a96dcb07e2ace69a
Jaegeuk Kim <jaegeuk.kim@samsung.com>
2014-02-04 14:20:16 +0900

    f2fs: support 3.10
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -1049,6 +1049,7 @@ repeat:
 		goto repeat;
 	}
 got_it:
+	mark_page_accessed(page);
 	return page;
 }
 
@@ -1103,6 +1104,7 @@ page_hit:
 		f2fs_put_page(page, 1);
 		return ERR_PTR(-EIO);
 	}
+	mark_page_accessed(page);
 	return page;
 }
 
@@ -1370,8 +1372,7 @@ static int f2fs_set_node_page_dirty(struct page *page)
 	return 0;
 }
 
-static void f2fs_invalidate_node_page(struct page *page, unsigned int offset,
-				      unsigned int length)
+static void f2fs_invalidate_node_page(struct page *page, unsigned long offset)
 {
 	struct inode *inode = page->mapping->host;
 	if (PageDirty(page))
