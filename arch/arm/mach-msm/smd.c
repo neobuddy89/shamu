6757356b54a716484ab3aebb6973fa1dfe4e755e
Jeff Hugo <jhugo@codeaurora.org>
2014-03-03 17:51:43 -0700

    soc: qcom: smd: Fix init irq race condition
    There is a small window where SMD could recieve an IRQ, but is not fully
    ready to process it yet.  This window could result in an access to invalid
    data.
    
    Avoid accesses to invalid data by not processing interrupts before the
    driver is fully ready to do so.  Any skipped activity will be processed at
    the end of init.
    
    Change-Id: If61a8d25502c69f0925a99ba5fc14c26c3542811
    Signed-off-by: Jeffrey Hugo <jhugo@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/mach-msm/smd.c
+++ arch/arm/mach-msm/smd.c
@@ -1503,6 +1503,8 @@ static inline void log_irq(uint32_t subsystem)
 
 irqreturn_t smd_modem_irq_handler(int irq, void *data)
 {
+	if (unlikely(!edge_to_pids[SMD_APPS_MODEM].initialized))
+		return IRQ_HANDLED;
 	log_irq(SMD_APPS_MODEM);
 	++interrupt_stats[SMD_MODEM].smd_in_count;
 	handle_smd_irq(&remote_info[SMD_MODEM], notify_modem_smd);
@@ -1512,6 +1514,8 @@ irqreturn_t smd_modem_irq_handler(int irq, void *data)
 
 irqreturn_t smd_dsp_irq_handler(int irq, void *data)
 {
+	if (unlikely(!edge_to_pids[SMD_APPS_QDSP].initialized))
+		return IRQ_HANDLED;
 	log_irq(SMD_APPS_QDSP);
 	++interrupt_stats[SMD_Q6].smd_in_count;
 	handle_smd_irq(&remote_info[SMD_Q6], notify_dsp_smd);
@@ -1521,6 +1525,8 @@ irqreturn_t smd_dsp_irq_handler(int irq, void *data)
 
 irqreturn_t smd_dsps_irq_handler(int irq, void *data)
 {
+	if (unlikely(!edge_to_pids[SMD_APPS_DSPS].initialized))
+		return IRQ_HANDLED;
 	log_irq(SMD_APPS_DSPS);
 	++interrupt_stats[SMD_DSPS].smd_in_count;
 	handle_smd_irq(&remote_info[SMD_DSPS], notify_dsps_smd);
@@ -1530,6 +1536,8 @@ irqreturn_t smd_dsps_irq_handler(int irq, void *data)
 
 irqreturn_t smd_wcnss_irq_handler(int irq, void *data)
 {
+	if (unlikely(!edge_to_pids[SMD_APPS_WCNSS].initialized))
+		return IRQ_HANDLED;
 	log_irq(SMD_APPS_WCNSS);
 	++interrupt_stats[SMD_WCNSS].smd_in_count;
 	handle_smd_irq(&remote_info[SMD_WCNSS], notify_wcnss_smd);
@@ -1539,6 +1547,8 @@ irqreturn_t smd_wcnss_irq_handler(int irq, void *data)
 
 irqreturn_t smd_modemfw_irq_handler(int irq, void *data)
 {
+	if (unlikely(!edge_to_pids[SMD_APPS_Q6FW].initialized))
+		return IRQ_HANDLED;
 	log_irq(SMD_APPS_Q6FW);
 	++interrupt_stats[SMD_MODEM_Q6_FW].smd_in_count;
 	handle_smd_irq(&remote_info[SMD_MODEM_Q6_FW], notify_modemfw_smd);
@@ -1548,6 +1558,8 @@ irqreturn_t smd_modemfw_irq_handler(int irq, void *data)
 
 irqreturn_t smd_rpm_irq_handler(int irq, void *data)
 {
+	if (unlikely(!edge_to_pids[SMD_APPS_RPM].initialized))
+		return IRQ_HANDLED;
 	log_irq(SMD_APPS_RPM);
 	++interrupt_stats[SMD_RPM].smd_in_count;
 	handle_smd_irq(&remote_info[SMD_RPM], notify_rpm_smd);
