661c59e92c5dbab81aa84f1914d14ada5ce5e7d7
Tarun Karra <tkarra@codeaurora.org>
2014-03-12 13:31:38 -0700

    msm: kgsl: Fix power api's used in fault interrupt sysfs control
    Sysfs control of fault interrupt is using incorrect GPU power
    control api's. *_clk api should not be used by rest of the
    GPU driver, use the correct api for consistency.
    
    Signed-off-by: Tarun Karra <tkarra@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.h
+++ drivers/gpu/msm/kgsl_pwrctrl.h
@@ -142,7 +142,6 @@ void kgsl_pwrctrl_uninit_sysfs(struct kgsl_device *device);
 void kgsl_pwrctrl_enable(struct kgsl_device *device);
 void kgsl_pwrctrl_disable(struct kgsl_device *device);
 bool kgsl_pwrctrl_isenabled(struct kgsl_device *device);
-bool kgsl_pwrrail_isenabled(struct kgsl_device *device);
 
 static inline unsigned long kgsl_get_clkrate(struct clk *clk)
 {
@@ -167,7 +166,5 @@ void kgsl_pwrctrl_request_state(struct kgsl_device *device, unsigned int state);
 int __must_check kgsl_active_count_get(struct kgsl_device *device);
 void kgsl_active_count_put(struct kgsl_device *device);
 int kgsl_active_count_wait(struct kgsl_device *device, int count);
-void kgsl_pwrctrl_clk(struct kgsl_device *device, int state,
-				int requested_state);
 
 #endif /* __KGSL_PWRCTRL_H */
