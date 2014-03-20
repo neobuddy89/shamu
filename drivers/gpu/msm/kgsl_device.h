e8841aff05355d27d4c4070802d4391874cc79d8
Jordan Crouse <jcrouse@codeaurora.org>
2014-03-20 08:49:18 -0600

    msm: ksgl: Add debugfs file to show active GPU events
    Add a debugfs file (/d/kgsl/events) to show the current list of
    active groups and events.
    
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/gpu/msm/kgsl_device.h
+++ drivers/gpu/msm/kgsl_device.h
@@ -194,6 +194,7 @@ struct kgsl_event {
  * @events: List of active GPU events
  * @group: Node for the master group list
  * @processed: Last processed timestamp
+ * @name: String name for the group (for the debugfs file)
  */
 struct kgsl_event_group {
 	struct kgsl_context *context;
@@ -201,6 +202,7 @@ struct kgsl_event_group {
 	struct list_head events;
 	struct list_head group;
 	unsigned int processed;
+	char name[64];
 };
 
 /**
@@ -578,7 +580,7 @@ void kgsl_events_exit(void);
 
 void kgsl_del_event_group(struct kgsl_event_group *group);
 void kgsl_add_event_group(struct kgsl_event_group *group,
-		struct kgsl_context *context);
+		struct kgsl_context *context, const char *name);
 
 void kgsl_cancel_events_timestamp(struct kgsl_device *device,
 		struct kgsl_event_group *group, unsigned int timestamp);
