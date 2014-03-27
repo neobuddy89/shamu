74ad58ab52dbc820bfde75b0c9a6893aa0f0d34f
Deepak Katragadda <dkatraga@codeaurora.org>
2014-03-27 15:10:51 -0700

    qcom: subsys-pil-tz: Support subsystem boot via non-PIL drivers
    Add functionality to the subsys-pil-tz driver to allow
    subsystems to be booted up by other drivers. Add the
    "qcom,pil-no-auth" flag to the individual subsystem dt
    entry to enable this feature. Doing so converts the
    PIL ops to no-ops.
    
    Signed-off-by: Deepak Katragadda <dkatraga@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/soc/qcom/subsys-pil-tz.c
+++ drivers/soc/qcom/subsys-pil-tz.c
@@ -527,6 +527,9 @@ static int pil_make_proxy_vote(struct pil_desc *pil)
 	struct pil_tz_data *d = desc_to_data(pil);
 	int rc;
 
+	if (d->subsys_desc.no_auth)
+		return 0;
+
 	rc = enable_regulators(pil->dev, d->proxy_regs, d->proxy_reg_count);
 	if (rc)
 		return rc;
@@ -557,6 +560,9 @@ static void pil_remove_proxy_vote(struct pil_desc *pil)
 {
 	struct pil_tz_data *d = desc_to_data(pil);
 
+	if (d->subsys_desc.no_auth)
+		return;
+
 	if (d->bus_client)
 		msm_bus_scale_client_update_request(d->bus_client, 0);
 
@@ -578,6 +584,9 @@ static int pil_init_image_trusted(struct pil_desc *pil,
 	dma_addr_t mdata_phys;
 	int ret;
 	DEFINE_DMA_ATTRS(attrs);
+
+	if (d->subsys_desc.no_auth)
+		return 0;
 
 	ret = scm_pas_enable_bw();
 	if (ret)
@@ -619,6 +628,9 @@ static int pil_mem_setup_trusted(struct pil_desc *pil, phys_addr_t addr,
 	u32 scm_ret = 0;
 	int ret;
 
+	if (d->subsys_desc.no_auth)
+		return 0;
+
 	request.proc = d->pas_id;
 	request.start_addr = addr;
 	request.len = size;
@@ -634,8 +646,12 @@ static int pil_auth_and_reset(struct pil_desc *pil)
 {
 	struct pil_tz_data *d = desc_to_data(pil);
 	int rc;
-	u32 proc = d->pas_id, scm_ret = 0;
+	u32 proc, scm_ret = 0;
 
+	if (d->subsys_desc.no_auth)
+		return 0;
+
+	proc = d->pas_id;
 	rc = enable_regulators(pil->dev, d->regs, d->reg_count);
 	if (rc)
 		return rc;
@@ -666,9 +682,13 @@ err_clks:
 static int pil_shutdown_trusted(struct pil_desc *pil)
 {
 	struct pil_tz_data *d = desc_to_data(pil);
-	u32 proc = d->pas_id, scm_ret = 0;
+	u32 proc, scm_ret = 0;
 	int rc;
 
+	if (d->subsys_desc.no_auth)
+		return 0;
+
+	proc = d->pas_id;
 	rc = enable_regulators(pil->dev, d->proxy_regs, d->proxy_reg_count);
 	if (rc)
 		return rc;
@@ -844,11 +864,8 @@ static int pil_tz_driver_probe(struct platform_device *pdev)
 	if (rc)
 		return -ENOENT;
 
-	rc = of_property_read_u32(pdev->dev.of_node, "qcom,pas-id", &d->pas_id);
-	if (rc) {
-		dev_err(&pdev->dev, "Failed to find the pas_id.\n");
-		return rc;
-	}
+	if (of_property_read_bool(pdev->dev.of_node, "qcom,pil-no-auth"))
+		d->subsys_desc.no_auth = true;
 
 	rc = of_property_read_string(pdev->dev.of_node, "qcom,firmware-name",
 				      &d->desc.name);
@@ -878,7 +895,15 @@ static int pil_tz_driver_probe(struct platform_device *pdev)
 	if (!rc)
 		d->desc.proxy_timeout = proxy_timeout;
 
-	scm_pas_init(MSM_BUS_MASTER_CRYPTO_CORE0);
+	if (!d->subsys_desc.no_auth) {
+		rc = of_property_read_u32(pdev->dev.of_node, "qcom,pas-id",
+								&d->pas_id);
+		if (rc) {
+			dev_err(&pdev->dev, "Failed to find the pas_id.\n");
+			return rc;
+		}
+		scm_pas_init(MSM_BUS_MASTER_CRYPTO_CORE0);
+	}
 
 	rc = pil_desc_init(&d->desc);
 	if (rc)
