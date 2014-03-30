f60f40b86f37c05cbd2af14b325d92166151a6a3
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-03-29 18:17:37 -0700

    sched: Make the scheduler aware of C-state for cpus
    C-state represents a power-state of a cpu. A cpu could have one or
    more C-states associated with it. C-state transitions are based on
    various factors (expected sleep time for example). "Deeper" C-states
    implies longer wakeup latencies.
    
    Scheduler needs to know wakeup latency associated with various C-states.
    Having this information allows the scheduler to make better decisions
    during task placement. For example:
    
    - Prefer an idle cpu that is in the least shallow C-state
    - Avoid waking up small tasks on a idle cpu unless it is in the least
      shallow C-state
    
    This patch introduces APIs in the scheduler that can be used by the
    architecture specific power-management driver to inform the scheduler
    about C-states for cpus.
    
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1906,6 +1906,8 @@ extern void do_set_cpus_allowed(struct task_struct *p,
 
 extern int set_cpus_allowed_ptr(struct task_struct *p,
 				const struct cpumask *new_mask);
+extern void sched_set_cpu_cstate(int cpu, int cstate,
+			 int wakeup_energy, int wakeup_latency);
 #else
 static inline void do_set_cpus_allowed(struct task_struct *p,
 				      const struct cpumask *new_mask)
@@ -1917,6 +1919,10 @@ static inline int set_cpus_allowed_ptr(struct task_struct *p,
 	if (!cpumask_test_cpu(0, new_mask))
 		return -EINVAL;
 	return 0;
+}
+static inline void
+sched_set_cpu_cstate(int cpu, int cstate, int wakeup_energy, int wakeup_latency)
+{
 }
 #endif
 
