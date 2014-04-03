ba9006def26047205b742ecf956a152e7b7a4aa7
Mulu He <muluhe@codeaurora.org>
2014-04-03 17:24:29 +0800

    usb: gadget: Enable HID function for charging mode
    Provide HID function for only charging mode, in this mode device
    enumerated as one input device.
    
    Change-Id: I769adf76807b8a28adcc298de0536fa779176016
    Signed-off-by: Mulu He <muluhe@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -77,6 +77,7 @@
 #include "f_uac1.c"
 #endif
 #include "f_ncm.c"
+#include "f_charger.c"
 
 MODULE_AUTHOR("Mike Lockwood");
 MODULE_DESCRIPTION("Android Composite USB Driver");
@@ -1848,6 +1849,19 @@ static struct android_usb_function ccid_function = {
 	.bind_config	= ccid_function_bind_config,
 };
 
+/* Charger */
+static int charger_function_bind_config(struct android_usb_function *f,
+						struct usb_configuration *c)
+{
+	return charger_bind_config(c);
+}
+
+static struct android_usb_function charger_function = {
+	.name		= "charging",
+	.bind_config	= charger_function_bind_config,
+};
+
+
 static int
 mtp_function_init(struct android_usb_function *f,
 		struct usb_composite_dev *cdev)
@@ -2907,6 +2921,7 @@ static struct android_usb_function *supported_functions[] = {
 	&midi_function,
 	&uasp_function,
 	&usbnet_function,
+	&charger_function,
 	NULL
 };
 
