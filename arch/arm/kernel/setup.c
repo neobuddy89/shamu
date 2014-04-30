b26d46b27b043d8b22045fdf5629fb671d58404a
Matthew Qin <yqin@codeaurora.org>
2014-04-30 11:16:14 +0800

    msm: soc: add Processor in cpu info
    Add 'Processor' info back to cpuinfo so that new kernel revision
    could be compatible with old userspace apps.
    
    Signed-off-by: Matthew Qin <yqin@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/kernel/setup.c
+++ arch/arm/kernel/setup.c
@@ -1022,6 +1022,8 @@ static int c_show(struct seq_file *m, void *v)
 	seq_printf(m, "Revision\t: %04x\n", system_rev);
 	seq_printf(m, "Serial\t\t: %08x%08x\n",
 		   system_serial_high, system_serial_low);
+	seq_printf(m, "Processor\t: %s rev %d (%s)\n",
+		   cpu_name, read_cpuid_id() & 15, elf_platform);
 
 	if (mach_cpuinfo_show)
 		mach_cpuinfo_show(m, v);
