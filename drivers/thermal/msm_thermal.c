a02fd5620efedd1d2594521529049bf765d15394
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2014-07-06 16:56:45 +0530

    msm: thermal: Online only cores, which are hotplugged by KTM
    On thermal hotplug clear event, online only cores which are offlined
    by KTM. During a thermal hotplug event, KTM evaluates the thermal
    hotplug status of the cpus and hotplugs or brings them up accordingly.
    It impacts cases where some cores are not cold boot yet or those cores
    are offlined due to non thermal reason.
    
    Change-Id: Ib4073be13c28c444ff6d322432b4ed46e10b1f08
    Signed-off-by: Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -1130,10 +1130,12 @@ static int __ref update_offline_cores(int val)
 {
 	uint32_t cpu = 0;
 	int ret = 0;
+	uint32_t previous_cpus_offlined = 0;
 
 	if (!core_control_enabled)
 		return 0;
 
+	previous_cpus_offlined = msm_thermal_info.cpus_offlined;
 	msm_thermal_info.cpus_offlined = msm_thermal_info.core_control_mask & val;
 
 	for_each_possible_cpu(cpu) {
@@ -1149,7 +1151,7 @@ static int __ref update_offline_cores(int val)
 					cpu, ret);
 			else
 				pr_debug("Offlined CPU%d\n", cpu);
-		} else if (online_core) {
+		} else if (online_core && (previous_cpus_offlined & BIT(cpu))) {
 #ifdef CONFIG_STATE_HELPER
 			thermal_notify(cpu, 1);
 #endif
