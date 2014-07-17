9221a62f40ae79f44e54639a6167b162ee04a1ea
Shubhraprakash Das <sadas@codeaurora.org>
2014-07-17 16:36:31 -0700

    msm: kgsl: Clear and set device started bit on soft reset
    When device is soft reset then the started flag which indicates that
    device is inactive needs to be reset and set again when the reset
    completes. This prevents the code from hitting a BUG while waiting
    for dispatcher to idle when the device is being reset from the
    dispatcher code.
    
    Signed-off-by: Shubhraprakash Das <sadas@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/gpu/msm/adreno.c
+++ drivers/gpu/msm/adreno.c
@@ -2682,6 +2682,8 @@ static int adreno_soft_reset(struct kgsl_device *device)
 	kgsl_pwrctrl_irq(device, KGSL_PWRFLAGS_OFF);
 
 	adreno_clear_gpu_fault(adreno_dev);
+	/* since device is oficially off now clear start bit */
+	clear_bit(ADRENO_DEVICE_STARTED, &adreno_dev->priv);
 
 	/* Delete the idle timer */
 	del_timer_sync(&device->idle_timer);
@@ -2725,6 +2727,7 @@ static int adreno_soft_reset(struct kgsl_device *device)
 		return ret;
 
 	device->reset_counter++;
+	set_bit(ADRENO_DEVICE_STARTED, &adreno_dev->priv);
 
 	return 0;
 }
