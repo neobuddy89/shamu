a907258098c3b5ad7b703b7c9a0dac155a7dc717
Anish Kumar <kanish@codeaurora.org>
2014-07-20 10:40:11 -0700

    ASoC: msm: qdsp6v2: Change pr_debug to pr_info
    Change pr_debug to pr_info for speaker protection
    logs as this will help in debugging. Remove
    un-needed log and redundant lines also.
    
    Signed-off-by: Anish Kumar <kanish@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- sound/soc/msm/qdsp6v2/q6afe.c
+++ sound/soc/msm/qdsp6v2/q6afe.c
@@ -90,7 +90,6 @@ void afe_set_aanc_info(struct aanc_data *q6_aanc_info)
 		this_afe.aanc_info.aanc_tx_port);
 }
 
-
 static int32_t afe_callback(struct apr_client_data *data, void *priv)
 {
 	int i;
@@ -225,7 +224,6 @@ static int32_t afe_callback(struct apr_client_data *data, void *priv)
 	}
 	return 0;
 }
-
 
 int afe_get_port_type(u16 port_id)
 {
@@ -3636,7 +3634,7 @@ int afe_spk_prot_get_calib_data(struct afe_spkr_prot_get_vi_calib *calib_resp)
 	}
 	memcpy(&calib_resp->res_cfg , &this_afe.calib_data.res_cfg,
 		sizeof(this_afe.calib_data.res_cfg));
-	pr_debug("%s: state %d resistance %d\n", __func__,
+	pr_info("%s: state %d resistance %d\n", __func__,
 			 calib_resp->res_cfg.th_vi_ca_state,
 			 calib_resp->res_cfg.r0_cali_q24);
 	ret = 0;
