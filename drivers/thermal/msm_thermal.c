df9825d6a99a85667262d0467ab8a94902233f60
Shiju Mathew <shijum@codeaurora.org>
2014-11-11 18:47:02 -0500

    msm: thermal: Proceed thermal late init only if probe is successful
    Skip thermal late init if thermal probe has not
    completed since all the tasks in late init depend
    on successful completion of thermal probe.
    
    Signed-off-by: Shiju Mathew <shijum@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -3385,6 +3385,9 @@ int __init msm_thermal_device_init(void)
 
 int __init msm_thermal_late_init(void)
 {
+	if (!msm_thermal_probed)
+		return 0;
+
 	if (num_possible_cpus() > 1)
 		msm_thermal_add_cc_nodes();
 	msm_thermal_add_psm_nodes();
