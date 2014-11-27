02e5308cde88b17b4f3904ca681fd66c08f0af2e
Shiju Mathew <shijum@codeaurora.org>
2014-11-27 16:14:01 -0500

    msm: thermal: Optimize locking in thermal frequency  mitigation thread
    Optimize locking in thermal frequency mitigation thread
    to reduce lock contention with hotplug mitigation thread.
    This lock is not explicitly required since the cpu driver
    already have a per cluster lock down the freqency mitigation
    call path. This would be much optimized since the new changes
    will hold the lock for a shorter period and unlike previously
    where the lock was for entire cluster, this is per cluster
    lock that will further reduce lock contention. This would
    help thermal to do mitigation faster.
    
    Signed-off-by: Shiju Mathew <shijum@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -1667,7 +1667,6 @@ static __ref int do_freq_mitigation(void *data)
 		else 
 			skip_mitig = false;
 
-		get_online_cpus();
 		for_each_possible_cpu(cpu) {
 			max_freq_req = (cpus[cpu].max_freq) ?
 					msm_thermal_info.freq_limit :
@@ -1698,7 +1697,6 @@ reset_threshold:
 				cpus[cpu].freq_thresh_clear = false;
 			}
 		}
-		put_online_cpus();
 	}
 
 	return ret;
