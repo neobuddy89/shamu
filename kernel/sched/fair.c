e9d3571b86e0a1b45f7a5f6ef0e2f038d3de8b10
Olav Haugan <ohaugan@codeaurora.org>
2014-12-06 10:09:43 -0800

    sched: Avoid frequent task migration due to EA in lb
    A new tunable exists that allow task migration to be throttled when the
    scheduler tries to do task migrations due to Energy Awareness (EA). This
    tunable is only taken into account when migrations occur in the tick
    path. Extend the usage of the tunable to take into account the load
    balancer (lb) path also.
    
    In addition ensure that the start of task execution on a CPU is updated
    correctly. If a task is preempted but still runnable on the same CPU the
    start of execution should not be updated. Only update the start of
    execution when a task wakes up after sleep or moves to a new CPU.
    
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1229,7 +1229,12 @@ unsigned int __read_mostly sched_init_task_load_pelt;
 unsigned int __read_mostly sched_init_task_load_windows;
 unsigned int __read_mostly sysctl_sched_init_task_load_pct = 15;
 
-unsigned int __read_mostly sysctl_sched_min_runtime = 200000000; /* 200 ms */
+/*
+ * Keep these two below in sync. One is in unit of ns and the
+ * other in unit of us.
+ */
+unsigned int __read_mostly sysctl_sched_min_runtime = 0; /* 0 ms */
+u64 __read_mostly sched_min_runtime = 0; /* 0 ms */
 
 static inline unsigned int task_load(struct task_struct *p)
 {
@@ -2348,10 +2353,12 @@ int sched_hmp_proc_update_handler(struct ctl_table *table, int write,
 	if (write && (old_val == *data))
 		goto done;
 
-	if (data == (unsigned int *)&sysctl_sched_upmigrate_min_nice)
-		update_min_nice = 1;
+	if (data == &sysctl_sched_min_runtime) {
+		sched_min_runtime = ((u64) sysctl_sched_min_runtime) * 1000;
+		goto done;
+	}
 
-	if (update_min_nice) {
+	if (data == (unsigned int *)&sysctl_sched_upmigrate_min_nice) {
 		if ((*(int *)data) < -20 || (*(int *)data) > 19) {
 			*data = old_val;
 			ret = -EINVAL;
@@ -2484,6 +2491,7 @@ static int lower_power_cpu_available(struct task_struct *p, int cpu)
 }
 
 static inline int is_cpu_throttling_imminent(int cpu);
+static inline int is_task_migration_throttled(struct task_struct *p);
 
 /*
  * Check if a task is on the "wrong" cpu (i.e its current cpu is not the ideal
@@ -2516,6 +2524,7 @@ static inline int migration_needed(struct rq *rq, struct task_struct *p)
 		return MOVE_TO_BIG_CPU;
 
 	if (sched_enable_power_aware &&
+	    !is_task_migration_throttled(p) &&
 	    is_cpu_throttling_imminent(cpu_of(rq)) &&
 	    lower_power_cpu_available(p, cpu_of(rq)))
 		return MOVE_TO_POWER_EFFICIENT_CPU;
@@ -2590,6 +2599,13 @@ static inline int is_cpu_throttling_imminent(int cpu)
 	return throttling;
 }
 
+static inline int is_task_migration_throttled(struct task_struct *p)
+{
+	u64 delta = sched_clock() - p->run_start;
+
+	return delta < sched_min_runtime;
+}
+
 #else	/* CONFIG_SCHED_HMP */
 
 #define sched_enable_power_aware 0
@@ -2641,6 +2657,11 @@ static inline int nr_big_tasks(struct rq *rq)
 }
 
 static inline int is_cpu_throttling_imminent(int cpu)
+{
+	return 0;
+}
+
+static inline int is_task_migration_throttled(struct task_struct *p)
 {
 	return 0;
 }
@@ -6253,6 +6274,8 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 				   struct sched_group *sg,
 				   struct sg_lb_stats *sgs)
 {
+	int cpu;
+
 	if (sched_boost() && !sds->busiest && sgs->sum_nr_running &&
 		(env->idle != CPU_NOT_IDLE) && (capacity(env->dst_rq) >
 		group_rq_capacity(sg))) {
@@ -6277,11 +6300,13 @@ static bool update_sd_pick_busiest(struct lb_env *env,
 	 * seen a busy group yet and we are close to throttling. We want to
 	 * prioritize spreading work over power optimization.
 	 */
+	cpu = cpumask_first(sched_group_cpus(sg));
 	if (!sds->busiest && (capacity(env->dst_rq) == group_rq_capacity(sg)) &&
 	    sgs->sum_nr_running && (env->idle != CPU_NOT_IDLE) &&
 	    power_cost_at_freq(env->dst_cpu, 0) <
-	    power_cost_at_freq(cpumask_first(sched_group_cpus(sg)), 0) &&
-	    is_cpu_throttling_imminent(cpumask_first(sched_group_cpus(sg)))) {
+	    power_cost_at_freq(cpu, 0) &&
+	    !is_task_migration_throttled(cpu_rq(cpu)->curr) &&
+	    is_cpu_throttling_imminent(cpu)) {
 		env->flags |= LBF_PWR_ACTIVE_BALANCE;
 		return true;
 	}
