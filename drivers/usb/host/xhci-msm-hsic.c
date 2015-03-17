3a94b7341db0fa8bac08a28792c7e15fe2ed14d9
franciscofranco <franciscofranco.1990@gmail.com>
2015-03-17 15:56:32 +0000

    drivers: disable multiple debug masks
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/usb/host/xhci-msm-hsic.c
+++ drivers/usb/host/xhci-msm-hsic.c
@@ -149,10 +149,10 @@ static struct dbg_data dbg_hsic = {
 	.ctrl_lck = __RW_LOCK_UNLOCKED(clck),
 	.data_idx = 0,
 	.data_lck = __RW_LOCK_UNLOCKED(dlck),
-	.log_payload = 1,
-	.log_events = 1,
-	.inep_log_mask = 1,
-	.outep_log_mask = 1
+	.log_payload = 0,
+	.log_events = 0,
+	.inep_log_mask = 0,
+	.outep_log_mask = 0
 };
 
 static inline void dbg_inc(unsigned *idx)
