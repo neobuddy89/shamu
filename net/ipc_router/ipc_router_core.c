f226da06a12948cdfe7350ac9bdbfc4a8f941b1d
Atish Kumar Patra <apatra@codeaurora.org>
2015-03-22 18:40:01 -0600

    net: ipc_router: Fix socket reference in IPC Router
    The socket associated with an IPC Router port can be released before
    the port is released. This leads to an use after free bug in cases
    where the socket is closed while a packet is in flight.
    
    Fix the use after free scenario by holding a sock reference during
    creation of the port and releasing that in release port function.
    
    CRs-Fixed: 811335
    Change-Id: I638cc59e3b4e2347107e5ac19f233b0f7b9dd7b0
    Signed-off-by: Atish Kumar Patra <apatra@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- net/ipc_router/ipc_router_core.c
+++ net/ipc_router/ipc_router_core.c
@@ -101,6 +101,8 @@ if (msm_ipc_router_debug_mask & R2R_RAW_HDR) \
 #define IPC_ROUTER_LOG_EVENT_RX         0x02
 #define IPC_ROUTER_DUMMY_DEST_NODE	0xFFFFFFFF
 
+#define ipc_port_sk(port) ((struct sock *)(port))
+
 static LIST_HEAD(control_ports);
 static DECLARE_RWSEM(control_ports_lock_lha5);
 
@@ -1079,6 +1081,8 @@ struct msm_ipc_port *msm_ipc_router_create_raw_port(void *endpoint,
 	port_ptr->priv = priv;
 
 	msm_ipc_router_add_local_port(port_ptr);
+	if (endpoint)
+		sock_hold(ipc_port_sk(endpoint));
 	return port_ptr;
 }
 
@@ -1125,6 +1129,8 @@ void ipc_router_release_port(struct kref *ref)
 	}
 	mutex_unlock(&port_ptr->port_rx_q_lock_lhc3);
 	wakeup_source_unregister(port_ptr->port_rx_ws);
+	if (port_ptr->endpoint)
+		sock_put(ipc_port_sk(port_ptr->endpoint));
 	kfree(port_ptr);
 }
 
