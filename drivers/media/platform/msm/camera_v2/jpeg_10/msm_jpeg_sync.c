8cad991fa12f924029e41368f376eb47dee45b3b
despairfactor <matthewalex@outlook.com>
2015-03-24 15:11:42 -0400

    build: GCC 5.0 cleanup
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/media/platform/msm/camera_v2/jpeg_10/msm_jpeg_sync.c
+++ drivers/media/platform/msm/camera_v2/jpeg_10/msm_jpeg_sync.c
@@ -78,7 +78,7 @@ struct msm_jpeg_buf32 {
 #endif
 
 
-inline void msm_jpeg_q_init(char const *name, struct msm_jpeg_q *q_p)
+void msm_jpeg_q_init(char const *name, struct msm_jpeg_q *q_p)
 {
 	JPEG_DBG("%s:%d] %s\n", __func__, __LINE__, name);
 	q_p->name = name;
@@ -88,7 +88,7 @@ inline void msm_jpeg_q_init(char const *name, struct msm_jpeg_q *q_p)
 	q_p->unblck = 0;
 }
 
-inline void *msm_jpeg_q_out(struct msm_jpeg_q *q_p)
+void *msm_jpeg_q_out(struct msm_jpeg_q *q_p)
 {
 	unsigned long flags;
 	struct msm_jpeg_q_entry *q_entry_p = NULL;
@@ -114,7 +114,7 @@ inline void *msm_jpeg_q_out(struct msm_jpeg_q *q_p)
 	return data;
 }
 
-inline int msm_jpeg_q_in(struct msm_jpeg_q *q_p, void *data)
+int msm_jpeg_q_in(struct msm_jpeg_q *q_p, void *data)
 {
 	unsigned long flags;
 
@@ -136,7 +136,7 @@ inline int msm_jpeg_q_in(struct msm_jpeg_q *q_p, void *data)
 	return 0;
 }
 
-inline int msm_jpeg_q_in_buf(struct msm_jpeg_q *q_p,
+int msm_jpeg_q_in_buf(struct msm_jpeg_q *q_p,
 	struct msm_jpeg_core_buf *buf)
 {
 	struct msm_jpeg_core_buf *buf_p;
@@ -154,7 +154,7 @@ inline int msm_jpeg_q_in_buf(struct msm_jpeg_q *q_p,
 	return 0;
 }
 
-inline int msm_jpeg_q_wait(struct msm_jpeg_q *q_p)
+int msm_jpeg_q_wait(struct msm_jpeg_q *q_p)
 {
 	long tm = MAX_SCHEDULE_TIMEOUT; /* 500ms */
 	int rc;
