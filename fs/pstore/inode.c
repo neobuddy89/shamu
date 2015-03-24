cefe47866ec8815d0a6259a3a4e6bf7cbd8aac91
Pranav Vashi <neobuddy89@gmail.com>
2015-03-24 23:54:31 +0530

    pstore: switch from snprintf to scnprintf
    snprintf can return more than the size of buffer, this case
    is not expected here, obviously. Use scnprintf instead.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/pstore/inode.c
+++ fs/pstore/inode.c
@@ -315,25 +315,25 @@ int pstore_mkfile(enum pstore_type_id type, char *psname, u64 id, int count,
 
 	switch (type) {
 	case PSTORE_TYPE_DMESG:
-		snprintf(name, PSTORE_NAMELEN, "dmesg-%s-%lld", psname, id);
+		scnprintf(name, PSTORE_NAMELEN, "dmesg-%s-%lld", psname, id);
 		break;
 	case PSTORE_TYPE_CONSOLE:
-		snprintf(name, PSTORE_NAMELEN, "console-%s-%lld", psname, id);
+		scnprintf(name, PSTORE_NAMELEN, "console-%s-%lld", psname, id);
 		break;
 	case PSTORE_TYPE_FTRACE:
-		snprintf(name, PSTORE_NAMELEN, "ftrace-%s-%lld", psname, id);
+		scnprintf(name, PSTORE_NAMELEN, "ftrace-%s-%lld", psname, id);
 		break;
 	case PSTORE_TYPE_MCE:
-		snprintf(name, PSTORE_NAMELEN, "mce-%s-%lld", psname, id);
+		scnprintf(name, PSTORE_NAMELEN, "mce-%s-%lld", psname, id);
 		break;
 	case PSTORE_TYPE_ANNOTATE:
-		snprintf(name, PSTORE_NAMELEN, "annotate-%s", psname);
+		scnprintf(name, PSTORE_NAMELEN, "annotate-%s", psname);
 		break;
 	case PSTORE_TYPE_UNKNOWN:
-		snprintf(name, PSTORE_NAMELEN, "unknown-%s-%lld", psname, id);
+		scnprintf(name, PSTORE_NAMELEN, "unknown-%s-%lld", psname, id);
 		break;
 	default:
-		snprintf(name, PSTORE_NAMELEN, "type%d-%s-%lld",
+		scnprintf(name, PSTORE_NAMELEN, "type%d-%s-%lld",
 			type, psname, id);
 		break;
 	}
