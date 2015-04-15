1d936f39f35b004cd75d5a1717e31262f45d724e
Pranav Vashi <neobuddy89@gmail.com>
2015-04-16 01:17:37 +0530

    cpufreq: cpu-boost: Disable boost when suspended
    * Use state notifier helper to determine state and disable
      boost driver when device is suspended.
    * This should help to minimize unnecessary spikes.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -57,6 +57,7 @@ static unsigned int sync_threshold;
 module_param(sync_threshold, uint, 0644);
 
 static bool input_boost_enabled;
+static bool suspended;
 
 static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
@@ -328,6 +329,9 @@ static int boost_migration_notify(struct notifier_block *nb,
 	unsigned long flags;
 	struct cpu_sync *s = &per_cpu(sync_info, mnd->dest_cpu);
 
+	if (suspended)
+		return NOTIFY_OK;
+
 	if (load_based_syncs && (mnd->load <= migration_load_threshold))
 		return NOTIFY_OK;
 
@@ -387,7 +391,8 @@ static void cpuboost_input_event(struct input_handle *handle,
 	u64 now;
 	unsigned int min_interval;
 
-	if (!input_boost_enabled || work_pending(&input_boost_work))
+	if (suspended || !input_boost_enabled ||
+		work_pending(&input_boost_work))
 		return;
 
 	now = ktime_to_us(ktime_get());
@@ -484,7 +489,7 @@ static int cpuboost_cpu_callback(struct notifier_block *cpu_nb,
 	case CPU_UP_CANCELED:
 		break;
 	case CPU_ONLINE:
-		if (!hotplug_boost || !input_boost_enabled ||
+		if (suspended || !hotplug_boost || !input_boost_enabled ||
 		     work_pending(&input_boost_work))
 			break;
 		pr_debug("Hotplug boost for CPU%d\n", (int)hcpu);
@@ -517,7 +522,11 @@ static int state_notifier_callback(struct notifier_block *this,
 {
 	switch (event) {
 		case STATE_NOTIFIER_ACTIVE:
+			suspended = false;
 			__wakeup_boost();
+			break;
+		case STATE_NOTIFIER_SUSPEND:
+			suspended = true;
 			break;
 		default:
 			break;
