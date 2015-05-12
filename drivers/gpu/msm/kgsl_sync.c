80685a64070e7e57aacbac817b4e9389c3022f7f
Rama Vaddula <rvaddula@codeaurora.org>
2015-05-12 11:56:21 -0700

    Revert "msm: kgsl: Fix double put for context in an error case"
    This reverts commit 97403a644d64901eae4f336f957601823ecb3965
    
    Change-Id: Iafe108559a03b0bcb704ccbad6e600e1c6358056
    Signed-off-by: Rama Vaddula <rvaddula@codeaurora.org>

--- drivers/gpu/msm/kgsl_sync.c
+++ drivers/gpu/msm/kgsl_sync.c
@@ -193,7 +193,6 @@ int kgsl_add_fence_event(struct kgsl_device *device,
 	return 0;
 
 unlock:
-        kgsl_context_put(context);
 	kgsl_mutex_unlock(&device->mutex, &device->mutex_owner);
 
 out:
@@ -203,6 +202,7 @@ out:
 	if (fence)
 		sync_fence_put(fence);
 
+	kgsl_context_put(context);
 	kfree(event);
 	return ret;
 }
