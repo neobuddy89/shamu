1ee3f601db2936febd200214d6833b7cd2a7395b
Jaegeuk Kim <jaegeuk@kernel.org>
2015-05-30 04:00:15 +0300

    f2fs: support 3.10
    Conflicts:
    	fs/f2fs/data.c
    	fs/f2fs/file.c
    	fs/f2fs/namei.c
    
    Change-Id: I2540749a14e6c9f1788a09a06afd586b691f0edd
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -1071,6 +1071,7 @@ repeat:
 		f2fs_put_page(page, 1);
 		goto repeat;
 	}
+	mark_page_accessed(page);
 	return page;
 }
 
@@ -1125,6 +1126,7 @@ page_hit:
 		f2fs_put_page(page, 1);
 		return ERR_PTR(-EIO);
 	}
+	mark_page_accessed(page);
 	return page;
 }
 
