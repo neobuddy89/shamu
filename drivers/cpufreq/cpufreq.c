8122c793e1f3ec60ad1f6609d86d1d325b40b58c
Viresh Kumar <viresh.kumar@linaro.org>
2015-08-03 08:36:16 +0530

    cpufreq: update user_policy.* on success
    'user_policy' caches properties of a policy that are set by userspace.
    And these must be updated only if cpufreq core was successful in
    updating them based on request from user space.
    
    In store_scaling_governor(), we are updating user_policy.policy and
    user_policy.governor even if cpufreq_set_policy() failed. That's
    incorrect.
    
    Fix this by updating user_policy.* only if we were successful in
    updating the properties.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -531,16 +531,15 @@ static ssize_t store_scaling_governor(struct cpufreq_policy *policy,
 	new_policy.max = new_policy.user_policy.max;
 
 	ret = cpufreq_set_policy(policy, &new_policy);
+	if (ret)
+		return ret;
 
 	policy->user_policy.policy = policy->policy;
 	policy->user_policy.governor = policy->governor;
 
 	sysfs_notify(&policy->kobj, NULL, "scaling_governor");
 
-	if (ret)
-		return ret;
-	else
-		return count;
+	return count;
 }
 
 /**
