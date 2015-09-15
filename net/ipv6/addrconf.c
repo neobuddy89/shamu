2b80a86d9b22587bcf559bee03e48cfafad06fc6
Martin KaFai Lau <kafai@fb.com>
2015-09-15 14:30:08 -0700

    ipv6: Avoid double dst_free
    It is a prep work to get dst freeing from fib tree undergo
    a rcu grace period.
    
    The following is a common paradigm:
    if (ip6_del_rt(rt))
    	dst_free(rt)
    
    which means, if rt cannot be deleted from the fib tree, dst_free(rt) now.
    1. We don't know the ip6_del_rt(rt) failure is because it
       was not managed by fib tree (e.g. DST_NOCACHE) or it had already been
       removed from the fib tree.
    2. If rt had been managed by the fib tree, ip6_del_rt(rt) failure means
       dst_free(rt) has been called already.  A second
       dst_free(rt) is not always obviously safe.  The rt may have
       been destroyed already.
    3. If rt is a DST_NOCACHE, dst_free(rt) should not be called.
    4. It is a stopper to make dst freeing from fib tree undergo a
       rcu grace period.
    
    This patch is to use a DST_NOCACHE flag to indicate a rt is
    not managed by the fib tree.
    
    Signed-off-by: Martin KaFai Lau <kafai@fb.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Reviewed-by: Yu Liu <e7664c@motorola.com>
    SLTApproved: Yu Liu <e7664c@motorola.com>
    Submit-Approved: Yu Liu <e7664c@motorola.com>
    Tested-by: Yu Liu <e7664c@motorola.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -4714,8 +4714,8 @@ static void __ipv6_ifa_notify(int event, struct inet6_ifaddr *ifp)
 		addrconf_leave_solict(ifp->idev, &ifp->addr);
 		dst_hold(&ifp->rt->dst);
 
-		if (ip6_del_rt(ifp->rt))
-			dst_free(&ifp->rt->dst);
+		ip6_del_rt(ifp->rt);
+
 		break;
 	}
 	atomic_inc(&net->ipv6.dev_addr_genid);
