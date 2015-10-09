9c50790c9529eee3e384348558b1297662b5919b
Kevin Cernekee <cernekee@chromium.org>
2015-10-09 14:48:19 -0700

    CHROMIUM: net: wireless: bcmdhd: Use a mutex for dhd_os_sdlock()
    Currently this uses a semaphore.  Semaphores aren't checked by lockdep,
    so deadlocks are not automatically detected.  Using a mutex will fix
    this.
    
    BUG=chrome-os-partner:46070
    TEST=manual
    
    Signed-off-by: Kevin Cernekee <cernekee@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/305031
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>
    Reviewed-by: Dmitry Shmidt <dimitrysh@google.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -44,6 +44,7 @@
 #include <linux/etherdevice.h>
 #include <linux/random.h>
 #include <linux/spinlock.h>
+#include <linux/mutex.h>
 #include <linux/ethtool.h>
 #include <linux/fcntl.h>
 #include <linux/fs.h>
@@ -393,7 +394,7 @@ typedef struct dhd_info {
 	spinlock_t	txqlock;
 	spinlock_t	dhd_lock;
 
-	struct semaphore sdsem;
+	struct mutex	sdmutex;
 	tsk_ctl_t	thr_dpc_ctl;
 	tsk_ctl_t	thr_wdt_ctl;
 
@@ -4802,7 +4803,7 @@ dhd_attach(osl_t *osh, struct dhd_bus *bus, uint bus_hdrlen)
 	dhd->thr_wdt_ctl.thr_pid = DHD_PID_KT_INVALID;
 
 	/* Initialize thread based operation and lock */
-	sema_init(&dhd->sdsem, 1);
+	mutex_init(&dhd->sdmutex);
 
 	/* Some DHD modules (e.g. cfg80211) configures operation mode based on firmware name.
 	 * This is indeed a hack but we have to make it work properly before we have a better
@@ -7316,7 +7317,7 @@ dhd_os_sdlock(dhd_pub_t *pub)
 	dhd = (dhd_info_t *)(pub->info);
 
 	if (dhd_dpc_prio >= 0)
-		down(&dhd->sdsem);
+		mutex_lock(&dhd->sdmutex);
 	else
 		spin_lock_bh(&dhd->sdlock);
 }
@@ -7329,7 +7330,7 @@ dhd_os_sdunlock(dhd_pub_t *pub)
 	dhd = (dhd_info_t *)(pub->info);
 
 	if (dhd_dpc_prio >= 0)
-		up(&dhd->sdsem);
+		mutex_unlock(&dhd->sdmutex);
 	else
 		spin_unlock_bh(&dhd->sdlock);
 }
