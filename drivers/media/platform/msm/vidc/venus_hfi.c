4c19e9b6973a616c87668945bead2ff0ba50b81e
Balamurugan Alagarsamy <balaga@codeaurora.org>
2016-01-14 09:35:30 +0530

    msm: vidc: initialize the session list when device initialized
    When multiple sessions are running and if there is a SYS_ERROR
    all clients will be notified with hardware error and clients
    are expected to close the sessions. If new session is opened
    before all sessions are closed results in list corruption
    because session open will do list initialization. To avoid
    list corruption issue initialize the session list at the time of
    device initialization instead of first session open.
    
    Signed-off-by: Balamurugan Alagarsamy <balaga@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/media/platform/msm/vidc/venus_hfi.c
+++ drivers/media/platform/msm/vidc/venus_hfi.c
@@ -2128,7 +2128,6 @@ static int venus_hfi_core_init(void *device)
 	venus_hfi_set_state(dev, VENUS_STATE_INIT);
 
 	dev->intr_status = 0;
-	INIT_LIST_HEAD(&dev->sess_head);
 	venus_hfi_set_registers(dev);
 
 	if (!dev->hal_client) {
@@ -3938,6 +3937,7 @@ static void *venus_hfi_add_device(u32 device_id,
 	mutex_init(&hdevice->session_lock);
 	mutex_init(&hdevice->resource_lock);
 
+	INIT_LIST_HEAD(&hdevice->sess_head);
 	if (hal_ctxt.dev_count == 0)
 		INIT_LIST_HEAD(&hal_ctxt.dev_head);
 
