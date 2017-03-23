a774e61d50dd3bbe51e11fe644af158ffea44147
Ram Chandrasekar <rkumbako@codeaurora.org>
2017-03-23 16:33:58 -0600

    msm: thermal-dev: Protect IOCTL from race condition
    There is a possibility that the thermal ioctl interface can be accessed
    simultaneously in a multi-threaded environment. In those cases the
    calls to fetch the frequency and voltage table can result in an
    undefined behavior due to race condition.
    
    Use mutex to protect the IOCTL interface from multi-thread access race
    condition.
    
    Change-Id: I325695f38753a4d4bc732987cf514e8616273aca
    Signed-off-by: Ram Chandrasekar <rkumbako@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/thermal/msm_thermal-dev.c
+++ drivers/thermal/msm_thermal-dev.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -31,6 +31,7 @@ struct msm_thermal_ioctl_dev {
 static int msm_thermal_major;
 static struct class *thermal_class;
 static struct msm_thermal_ioctl_dev *msm_thermal_dev;
+static DEFINE_MUTEX(ioctl_access_mutex);
 
 static int msm_thermal_ioctl_open(struct inode *node, struct file *filep)
 {
@@ -115,8 +116,9 @@ static long msm_thermal_ioctl_process(struct file *filep, unsigned int cmd,
 
 	ret = validate_and_copy(&cmd, &arg, &query);
 	if (ret)
-		goto process_exit;
+		return ret;
 
+	mutex_lock(&ioctl_access_mutex);
 	switch (cmd) {
 	case MSM_THERMAL_SET_CPU_MAX_FREQUENCY:
 		ret = msm_thermal_set_frequency(query.cpu_freq.cpu_num,
@@ -131,6 +133,7 @@ static long msm_thermal_ioctl_process(struct file *filep, unsigned int cmd,
 		goto process_exit;
 	}
 process_exit:
+	mutex_unlock(&ioctl_access_mutex);
 	return ret;
 }
 
