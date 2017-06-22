neobuddy89/shamu

commit 248f01ccd3b1c0bd600a06568d6404c374e66dc4

msm: Use 64 bit type for sclk
'us' can overflow and can potentially cause unexpected wakeup.

Change it to uint64_t.

Change-Id: I943cbc9d62268ca073e388c287e3b180c0eaa8e3
Signed-off-by: Girish S Ghongdemath <girishsg@codeaurora.org>
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

arch/arm/mach-msm/lpm_levels.c
@@ -354,7 +354,7 @@ static void lpm_system_prepare(struct lpm_system_state *system_state,
 	struct lpm_system_level *lvl;
 	uint32_t sclk;
 	int ret;
-	int64_t us = (~0ULL);
+	uint64_t us = (~0ULL);
 	int dbg_mask;
 	struct cpumask nextcpu;
 
@@ -392,7 +392,7 @@ static void lpm_system_prepare(struct lpm_system_state *system_state,
 	}
 
 	do_div(us, USEC_PER_SEC/SCLK_HZ);
-	sclk = (uint32_t)us;
+	sclk = us;
 	msm_mpm_enter_sleep(sclk, from_idle, &nextcpu);
 	if (from_idle)
 		time = ktime_to_ns(ktime_get());

arch/arm/mach-msm/mpm-of.c
@@ -520,7 +520,7 @@ bool msm_mpm_irqs_detectable(bool from_idle)
 			from_idle);
 }
 
-void msm_mpm_enter_sleep(uint32_t sclk_count, bool from_idle,
+void msm_mpm_enter_sleep(uint64_t sclk_count, bool from_idle,
 		const struct cpumask *cpumask)
 {
 	cycle_t wakeup = (u64)sclk_count * ARCH_TIMER_HZ;

include/linux/irqchip/msm-mpm-irq.h
@@ -98,7 +98,7 @@ bool msm_mpm_gpio_irqs_detectable(bool from_idle);
  * Low power management code calls into this API to configure the MPM to
  * monitor the active irqs before going to sleep.
  */
-void msm_mpm_enter_sleep(uint32_t sclk_count, bool from_idle,
+void msm_mpm_enter_sleep(uint64_t sclk_count, bool from_idle,
 		const struct cpumask *cpumask);
 /**
  * msm_mpm_exit_sleep() -Called from PM code after resuming from low power mode
@@ -140,7 +140,7 @@ static inline bool msm_mpm_irqs_detectable(bool from_idle)
 { return false; }
 static inline bool msm_mpm_gpio_irqs_detectable(bool from_idle)
 { return false; }
-static inline void msm_mpm_enter_sleep(uint32_t sclk_count, bool from_idle,
+static inline void msm_mpm_enter_sleep(uint64_t sclk_count, bool from_idle,
 		const struct cpumask *cpumask) {}
 static inline void msm_mpm_exit_sleep(bool from_idle) {}
 static inline void __init of_mpm_init(void) {}

commit 4c85b56f4416e435cf8938f705007e1080b2777a

msm: lpm-levels: Fix truncation of wakeup time
* Fixes: 248f01ccd3b1c0bd600a06568d6404c374e66dc4

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

arch/arm/mach-msm/lpm_levels.c
@@ -353,7 +353,6 @@ static void lpm_system_prepare(struct lpm_system_state *system_state,
 		int index, bool from_idle)
 {
 	struct lpm_system_level *lvl;
-	uint32_t sclk;
 	int ret;
 	uint64_t us = (~0ULL);
 	int dbg_mask;
@@ -393,8 +392,7 @@ static void lpm_system_prepare(struct lpm_system_state *system_state,
 	}
 
 	do_div(us, USEC_PER_SEC/SCLK_HZ);
-	sclk = us;
-	msm_mpm_enter_sleep(sclk, from_idle, &nextcpu);
+	msm_mpm_enter_sleep(us, from_idle, &nextcpu);
 	if (from_idle)
 		time = ktime_to_ns(ktime_get());
 skip_rpm: